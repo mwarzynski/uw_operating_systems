06f7fb62d08825c0212657aa6ab743810467fa23
Mateusz Warzyński <mathew.warzynski@gmail.com>
2017-03-29 19:17:48 +0200

    add second bootloadeer

--- scripts/injectmbr.sh
+++ scripts/injectmbr.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
-
 cp snapshots/minix.img minix.img
 nasm -f bin bootloader.asm
+nasm -f bin bootloader2.asm
+
 ./scripts/run.sh &
+
 sleep 15
+
 scp bootloader minix:~/bootloader
+scp bootloader2 minix:~/bootloader2
+
 ssh minix << "ENDSSH"
-dd bs=512 count=1 if=bootloader of=/dev/c0d0 && reboot
+dd bs=512 count=1 if=/dev/c0d0  of=orig_bootloader.bin &&
+dd bs=512 seek=1 count=1 if=orig_bootloader of=/dev/c0d0 &&
+dd bs=512 count=1 if=bootloader of=/dev/c0d0 &&
+dd bs=512 seek=2 count=1 if=bootloader2 of=/dev/c0d0 &&
+reboot
 ENDSSH
+
+rm bootloader
+rm bootloader2
