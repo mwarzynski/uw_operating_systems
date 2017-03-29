7be1491d70a45f194700d19a83153cfeaf192532
Mateusz Warzyński <mathew.warzynski@gmail.com>
2017-03-29 18:38:28 +0200

    added printing message

--- bootloader.asm
+++ bootloader.asm
@@ -1,30 +1,37 @@
-USERNAME_MSG: db 'Type you username: '
-BUFFER times 64 db 0
-
 
 start:
     mov ax, cs
     mov ds, ax
-    mov ex, ax
+    mov es, ax
     mov ss, ax 
     mov sp, 0x8000
 
-    mov al, 'H'
-    call print_char
+    mov ax, USERNAME_MSG
+    call print
 
 loop:
     jmp loop
 
 print:
-    ax         ; buffer
-    xor cx, cx ; index
-    .print_t
-    mov bx, [ax + cx]
+    mov bx, ax
+
+print_text:
+    mov al, byte [bx]
+    test al, al
+    je print_ret
+    call print_char
+    inc bx
+    jmp print_text
+
+print_ret:
+    ret
 
 print_char:
     mov ah, 0xe
     int 0x10
     ret
+
+USERNAME_MSG: db 'Type you username: '
 
 times (510 - ($ - $$)) db 0;
 dw 0xaa55
