cc5a7fc3ca718c0a8b54381e70c6c984cbda50cd
Mateusz Warzyński <mathew.warzynski@gmail.com>
2017-04-02 23:24:46 +0200

    rename scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,23 @@
-#!/bin/bash
+#!/bin/sh
+cp snapshots/minix.img minix.img
+nasm -f bin bootloader.asm
+nasm -f bin bootloader2.asm
 
-qemu-system-x86_64 -enable-kvm -drive file=minix.img -localtime -net user,hostfwd=tcp::10022-:22 -net nic,model=virtio -m 1024M
+./scripts/qemu.sh &
+
+sleep 5
+
+scp bootloader minix:~/bootloader
+scp bootloader2 minix:~/bootloader2
+
+rm bootloader
+rm bootloader2
+
+ssh minix << "ENDSSH"
+dd bs=512 count=1 if=/dev/c0d0  of=orig_bootloader &&
+dd bs=512 count=1 if=bootloader of=/dev/c0d0 &&
+dd bs=512 seek=1 count=1 if=orig_bootloader of=/dev/c0d0 &&
+dd bs=512 seek=2 count=1 if=bootloader2 of=/dev/c0d0 &&
+reboot
+ENDSSH
+
