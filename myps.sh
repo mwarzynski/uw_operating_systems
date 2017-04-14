e5d477f2e3e14edaffc5ca858788964acf8d823b
Mateusz Warzyński <mathew.warzynski@gmail.com>
2017-04-15 00:48:08 +0200

    Remove redundant building

--- myps.sh
+++ myps.sh
@@ -8,22 +8,14 @@ cp table.c  /usr/src/minix/servers/pm/table.c
 cp Makefile /usr/src/minix/servers/pm/Makefile
 
 # Copy source file for lib function
+cp unistd.h     /usr/include/unistd.h
+cp unistd.h     /usr/src/include/unistd.h
 cp Makefile.inc /usr/src/lib/libc/misc/
-cp func_myps.c /usr/src/lib/libc/misc/myps.c
+cp func_myps.c  /usr/src/lib/libc/misc/myps.c
 
-# Build kernel
+# Build everything
 cd /usr/src/releasetools
 make hdboot
-
-# Build PM
-cd /usr/src/minix/servers/pm
-make
-make install
-
-# Build libc
-cd /usr/src/lib/libc
-make
-make install
 
 # Reboot
 reboot
