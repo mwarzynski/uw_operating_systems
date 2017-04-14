e1fbe6de44ad11243e5b499ca1d30ee30a50aaac
Mateusz Warzyński <mathew.warzynski@gmail.com>
2017-04-14 15:59:12 +0200

    Clear task 3 code.

--- profile
+++
@@ -1,8 +0,0 @@
-dd bs=512 skip=3 count=1 if=/dev/c0d0 of=username 2> /dev/null
-USERNAME=$(head -c 13 username)
-
-echo "Adding user $USERNAME."
-useradd -G users -m $USERNAME
-
-echo "Logging in as $USERNAME."
-login -f $USERNAME
--- scripts/run.sh
+++ scripts/run.sh
@@ -3,10 +3,6 @@
 # Revert MINIX image to last clean one.
 cp snapshots/minix.img minix.img
 
-# Compile bootloaders to binary files.
-nasm -f bin bootloader.asm
-nasm -f bin bootloader2.asm
-
 # Run MINIX.
 ./scripts/qemu.sh > /dev/null 2> /dev/null &
 
@@ -14,17 +10,10 @@ nasm -f bin bootloader2.asm
 sleep 5
 
 # Copy files to MINIX.
-scp bootloader minix:~/bootloader
-scp bootloader2 minix:~/bootloader2
-scp profile minix:~/profile
-scp injector.sh minix:~/injector.sh
-
-# Clean up binary files.
-rm bootloader
-rm bootloader2
+#scp bootloader minix:~/bootloader
 
 # Execute injector.
 ssh minix << "ENDSSH"
-sh injector.sh 
+poweroff
 ENDSSH
 
