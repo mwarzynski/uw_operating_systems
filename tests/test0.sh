fd176ef5b79aa2ed5aa96eeeeae216f06849496e
Mateusz Warzyński <mathew.warzynski@gmail.com>
2017-05-30 20:03:26 +0200

    update qemu

--- tests/test0.sh
+++ tests/test0.sh
@@ -1,3 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "-- OK --"
+# Check if writing zeroes work
+
+in=$(dd if=/dev/zero bs=4096 count=5 2> /dev/null)
+echo $in > zero_file
+out=$(cat zero_file)
+
+result=$(diff <(echo "$in") <(echo "$out"))
+if [ "$result" = "" ]; then
+    echo "-- OK --"
+else
+    echo "-- ERR -- saved filed with zeros is not the same"
+fi
+rm zero_file
+
+
+# Check if writing random characters work
+
+in=$(dd if=/dev/urandom bs=4096 count=5 2> /dev/null | tr -dc '0-9a-zA-Z!@#$%^&*_+-')
+echo $in > random_file
+out=$(cat random_file)
+
+result=$(diff <(echo "$in") <(echo "$out"))
+if [ "$result" = "" ]; then
+    echo "-- OK --"
+else
+    echo "-- ERR -- saved filed with random values is not the same"
+fi
+rm random_file
+
