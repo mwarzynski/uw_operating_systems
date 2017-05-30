469dc68296532dfb3aeb77aacb2f51aedcc4bab3
Mateusz Warzyński <mathew.warzynski@gmail.com>
2017-05-30 20:03:26 +0200

    update qemu

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-qemu-system-x86_64 --enable-kvm -cpu host -drive file=minix.img -localtime -net user,hostfwd=tcp::10022-:22 -net nic,model=virtio -m 1024M
+qemu-system-x86_64 -drive file=minix.img -localtime -net user,hostfwd=tcp::10022-:22 -net nic,model=virtio -m 1024M
